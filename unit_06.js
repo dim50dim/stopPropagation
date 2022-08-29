b9aace1faf73ff43a2dca22e9bc2eeb89b8e5031
dim50dim <zheliezkov@gmail.com>
2022-08-29 14:26:09 +0300

    new tricks

--- unit_06.js
+++ unit_06.js
@@ -1,23 +1,12 @@
 document.querySelector('.questions').onclick = (event) => {
       event.stopPropagation();
    const li = event.target;
-
+   
   if(li.nodeName !== 'LI') return;
+
   if(li.getAttribute('data') === '111'){
-    li.classList.toggle('bg-orange');
+    li.classList.toggle('bg-orange');          
   }else {
     li.classList.toggle('bg-blue');
-
   }
-    //  console.log(li);
-
-    //   console.log(e.target);
-    //   let data = document.querySelectorAll('LI');
-    //   if(data.getAttribute('data') === '111'){
-    //     data.getAttribute('data').classList.toggle('bg-orange');
-
-    //   } else{
-    //     data.getAttribute('data').classList.toggle('bg-blue');
-    //   }
-
 }
